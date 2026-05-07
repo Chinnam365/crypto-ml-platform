b2a9fa5522df22433f505e398e801d46ef2820a0
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-05-07 17:19:13 +0100

    Add Doge strategy evaluation to route

--- src/routes/doge.js
+++ src/routes/doge.js
@@ -7,6 +7,8 @@ const { formatCandles } = require("../market/formatter");
 const { calculateEMA } = require("../indicators/ema");
 const { calculateRSI } = require("../indicators/rsi");
 
+const { evaluateDogeStrategy } = require("../strategies/dogeStrategy");
+
 router.get("/", async (req, res) => {
   try {
     const rawCandles = await getDogeCandles();
@@ -20,17 +22,24 @@ router.get("/", async (req, res) => {
 
     const rsi = calculateRSI(closes.slice(-15));
 
+    const strategyResult = evaluateDogeStrategy({
+      ema20,
+      ema50,
+      rsi,
+      latestPrice: closes[closes.length - 1],
+    });
+
     res.json({
       symbol: "DOGEUSDT",
       interval: "5m",
-
-      latestPrice: closes[closes.length - 1],
 
       indicators: {
         ema20,
         ema50,
         rsi,
       },
+
+      strategy: strategyResult,
 
       latestCandle: candles[candles.length - 1],
     });

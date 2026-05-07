617edf1b59f147e398d585315b2da020d80d2646
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-05-07 17:05:29 +0100

    Add doge route to the Express app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const dogeRoute = require("./routes/doge");
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Crypto ML Platform Running");
 });
+
+app.use("/api/doge", dogeRoute);
 
 module.exports = app;

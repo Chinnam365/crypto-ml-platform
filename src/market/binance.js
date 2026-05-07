d4dfb8512d984bcf6e05d6fd1acef66c06a27b2d
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-05-07 22:43:30 +0100

    Update binance.js

--- src/market/binance.js
+++ src/market/binance.js
@@ -1,22 +1,27 @@
 const axios = require("axios");
 
-async function getCandles(symbol, interval = "5m", limit = 50) {
+async function getCandles(
+  symbol,
+  interval = "5m",
+  limit = 100
+) {
   try {
-    const response = await axios.get(
-      "https://api.binance.com/api/v3/klines",
-      {
-        params: {
-          symbol,
-          interval,
-          limit,
-        },
-      }
-    );
+    const response =
+      await axios.get(
+        "https://api.binance.com/api/v3/klines",
+        {
+          params: {
+            symbol,
+            interval,
+            limit,
+          },
+        }
+      );
 
     return response.data;
   } catch (error) {
     console.error(
-      `Binance API Error (${symbol}):`,
+      `Binance API Error (${symbol} ${interval}):`,
       error.message
     );
 
@@ -24,16 +29,50 @@ async function getCandles(symbol, interval = "5m", limit = 50) {
   }
 }
 
-async function getDogeCandles() {
-  return getCandles("DOGEUSDT");
+// =========================
+// DOGE
+// =========================
+
+async function getDoge5mCandles() {
+  return getCandles(
+    "DOGEUSDT",
+    "5m"
+  );
 }
 
-async function getBtcCandles() {
-  return getCandles("BTCUSDT", "15m");
+async function getDoge15mCandles() {
+  return getCandles(
+    "DOGEUSDT",
+    "15m"
+  );
+}
+
+async function getDoge1hCandles() {
+  return getCandles(
+    "DOGEUSDT",
+    "1h"
+  );
+}
+
+// =========================
+// BTC
+// =========================
+
+async function getBtc15mCandles() {
+  return getCandles(
+    "BTCUSDT",
+    "15m"
+  );
 }
 
 module.exports = {
   getCandles,
-  getDogeCandles,
-  getBtcCandles,
+
+  getDoge5mCandles,
+
+  getDoge15mCandles,
+
+  getDoge1hCandles,
+
+  getBtc15mCandles,
 };

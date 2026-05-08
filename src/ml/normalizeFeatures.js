efcbc138d52ec8870f0772619521aa4bbba9e0b2
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-05-08 17:50:30 +0100

    Create normalizeFeatures.js

---
+++ src/ml/normalizeFeatures.js
@@ -0,0 +1,60 @@
+function normalizeFeatures(features) {
+
+  return {
+
+    rsi:
+      features.rsi / 100,
+
+    volatility:
+      features.volatility * 1000,
+
+    score:
+      features.score / 10,
+
+    bullish5m:
+      features.bullish5m,
+
+    bullish15m:
+      features.bullish15m,
+
+    bullish1h:
+      features.bullish1h,
+
+    btcBullish:
+      features.btcBullish,
+
+    ema5mSpread:
+      features.ema5mSpread * 1000,
+
+    ema15mSpread:
+      features.ema15mSpread * 1000,
+
+    ema1hSpread:
+      features.ema1hSpread * 1000,
+
+    atr:
+      (features.atr || 0) * 100,
+
+    candleBody:
+      (features.candleBody || 0) * 100,
+
+    upperWick:
+      (features.upperWick || 0) * 100,
+
+    lowerWick:
+      (features.lowerWick || 0) * 100,
+
+    emaSlope:
+      (features.emaSlope || 0) * 1000,
+
+    rsiSlope:
+      (features.rsiSlope || 0) / 10,
+
+    distanceFromEma:
+      (features.distanceFromEma || 0) * 1000,
+  };
+}
+
+module.exports = {
+  normalizeFeatures,
+};

f36b505ce4bc5a30633af41622470e1fa6c6add6
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-05-25 16:43:21 +0100

    Update saveTradeMemory.js

--- src/ml/saveTradeMemory.js
+++ src/ml/saveTradeMemory.js
@@ -83,14 +83,18 @@ async function saveTradeMemory(
         overall_trend,
         buy_score,
         sell_score
-
+trend_15m,
+trend_1h,
+trend_4h,
+alignment_score
       )
 
       VALUES (
 
         $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,
 
-        $11,$12,$13,$14,$15,$16
+        $11,$12,$13,$14,$15,$16,
+        $17,$18,$19,$20
       )
       `,
 
@@ -127,6 +131,13 @@ async function saveTradeMemory(
         trade.buyScore,
 
         trade.sellScore,
+        trade.trend15m,
+
+trade.trend1h,
+
+trade.trend4h,
+
+trade.alignmentScore,
       ]
     );
 

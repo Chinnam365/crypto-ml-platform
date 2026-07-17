be6182a9dd58305641e603d072a96d476ce94906
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-07-17 16:24:19 +0100

    Update index.js

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,15 +1,39 @@
 "use strict";
 
-const router = require("./authRoutes");
+const moduleInstance = require("./authModule");
+const bootstrap = require("./authBootstrap");
+const controller = require("./authController");
 const service = require("./authService");
 const repository = require("./authRepository");
-const controller = require("./authController");
+const routes = require("./authRoutes");
+const middleware = require("./authMiddleware");
 const constants = require("./authConstants");
+const health = require("./authHealthService");
 
 module.exports = Object.freeze({
-  router,
-  service,
-  repository,
+
+  initialize: async () => {
+    return moduleInstance.initialize();
+  },
+
+  shutdown: async () => {
+    return moduleInstance.shutdown();
+  },
+
+  router: routes,
+
+  bootstrap,
+
   controller,
-  constants
+
+  service,
+
+  repository,
+
+  middleware,
+
+  constants,
+
+  health
+
 });

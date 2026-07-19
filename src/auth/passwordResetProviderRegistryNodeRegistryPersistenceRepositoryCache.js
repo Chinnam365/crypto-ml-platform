db7e5c862a7557d844318130861164cc2a8a8484
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-07-19 15:08:34 +0100

    Create passwordResetProviderRegistryNodeRegistryPersistenceRepositoryCache.js

---
+++ src/auth/passwordResetProviderRegistryNodeRegistryPersistenceRepositoryCache.js
@@ -0,0 +1,82 @@
+"use strict";
+
+const PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryConstants = require("./passwordResetProviderRegistryNodeRegistryPersistenceRepositoryConstants");
+
+class PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryCache {
+  constructor(options = {}) {
+    this.namespace =
+      options.namespace ||
+      PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryConstants.DEFAULT_NAMESPACE;
+
+    this.cache = new Map();
+
+    this.maxEntries =
+      Number.isInteger(options.maxEntries) && options.maxEntries > 0
+        ? options.maxEntries
+        : 1000;
+  }
+
+  set(key, value) {
+    if (this.cache.size >= this.maxEntries && !this.cache.has(key)) {
+      const oldestKey = this.cache.keys().next().value;
+      this.cache.delete(oldestKey);
+    }
+
+    this.cache.set(key, {
+      value,
+      cachedAt: new Date().toISOString()
+    });
+
+    return value;
+  }
+
+  get(key) {
+    const entry = this.cache.get(key);
+    return entry ? entry.value : undefined;
+  }
+
+  has(key) {
+    return this.cache.has(key);
+  }
+
+  delete(key) {
+    return this.cache.delete(key);
+  }
+
+  clear() {
+    this.cache.clear();
+  }
+
+  size() {
+    return this.cache.size;
+  }
+
+  keys() {
+    return [...this.cache.keys()];
+  }
+
+  values() {
+    return [...this.cache.values()].map((entry) => entry.value);
+  }
+
+  entries() {
+    return [...this.cache.entries()].map(([key, entry]) => [
+      key,
+      entry.value
+    ]);
+  }
+
+  status() {
+    return {
+      component:
+        "PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryCache",
+      namespace: this.namespace,
+      entries: this.size(),
+      maxEntries: this.maxEntries,
+      timestamp: new Date().toISOString()
+    };
+  }
+}
+
+module.exports =
+  PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryCache;

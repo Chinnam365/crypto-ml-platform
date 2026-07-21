86a7878f47a220510da60e609e4dc49323f54b56
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-07-21 13:25:03 +0100

    Create passwordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryRepositoryEventBus.js

---
+++ src/auth/passwordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryRepositoryEventBus.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const { EventEmitter } = require("events");
+
+class PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryRepositoryEventBus extends EventEmitter {
+  constructor() {
+    super();
+    this.setMaxListeners(100);
+  }
+
+  publish(event, payload = {}) {
+    const message = {
+      event,
+      payload,
+      timestamp: new Date().toISOString()
+    };
+
+    this.emit(event, message);
+
+    return message;
+  }
+
+  subscribe(event, listener) {
+    this.on(event, listener);
+
+    return () => this.off(event, listener);
+  }
+
+  subscribeOnce(event, listener) {
+    this.once(event, listener);
+  }
+
+  unsubscribe(event, listener) {
+    this.off(event, listener);
+  }
+
+  clear(event) {
+    if (event) {
+      this.removeAllListeners(event);
+    } else {
+      this.removeAllListeners();
+    }
+  }
+
+  status() {
+    return {
+      component:
+        "PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryRepositoryEventBus",
+      healthy: true,
+      events: this.eventNames(),
+      timestamp: new Date().toISOString()
+    };
+  }
+}
+
+module.exports =
+  PasswordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryRepositoryEventBus;

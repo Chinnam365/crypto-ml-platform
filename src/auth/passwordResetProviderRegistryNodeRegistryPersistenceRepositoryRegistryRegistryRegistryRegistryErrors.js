4a72b14abd94609954d7ac0f0826e82eba0838cc
Chinnam365 <65852177+Chinnam365@users.noreply.github.com>
2026-07-21 13:13:04 +0100

    Create passwordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryErrors.js

---
+++ src/auth/passwordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryErrors.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const Constants = require("./passwordResetProviderRegistryNodeRegistryPersistenceRepositoryRegistryRegistryRegistryRegistryConstants");
+
+class RegistryError extends Error {
+  constructor(code, message, details = null) {
+    super(message);
+
+    this.name = this.constructor.name;
+    this.code = code;
+    this.details = details;
+    this.timestamp = new Date().toISOString();
+
+    Error.captureStackTrace?.(this, this.constructor);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      code: this.code,
+      message: this.message,
+      details: this.details,
+      timestamp: this.timestamp
+    };
+  }
+}
+
+class RegistryAlreadyExistsError extends RegistryError {
+  constructor(name) {
+    super(
+      Constants.ERRORS.REGISTRY_ALREADY_EXISTS,
+      `Registry '${name}' already exists.`,
+      { registry: name }
+    );
+  }
+}
+
+class RegistryNotFoundError extends RegistryError {
+  constructor(name) {
+    super(
+      Constants.ERRORS.REGISTRY_NOT_FOUND,
+      `Registry '${name}' was not found.`,
+      { registry: name }
+    );
+  }
+}
+
+class InvalidRegistryError extends RegistryError {
+  constructor(registry) {
+    super(
+      Constants.ERRORS.INVALID_REGISTRY,
+      "Invalid registry supplied.",
+      { registry }
+    );
+  }
+}
+
+class InvalidRegistryNameError extends RegistryError {
+  constructor(name) {
+    super(
+      Constants.ERRORS.INVALID_NAME,
+      "Invalid registry name supplied.",
+      { registry: name }
+    );
+  }
+}
+
+module.exports = {
+  RegistryError,
+  RegistryAlreadyExistsError,
+  RegistryNotFoundError,
+  InvalidRegistryError,
+  InvalidRegistryNameError
+};
